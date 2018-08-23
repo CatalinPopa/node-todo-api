6f87a93741623fa1de98821417a263a3fefeac7e
Catalin Popa <catalin.s.popa@gmail.com>
2018-08-23 16:27:14 +0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDb server.');
+  }
+  console.log('Connected to MongoDb server.');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b7e8f272cab40dd3950564e')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
