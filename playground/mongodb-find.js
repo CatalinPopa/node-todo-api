ba5a2829fc6b9acc9e930a1705492ac0eb856279
Catalin Popa <Catalin.Popa@Yardi.com>
2018-08-23 15:47:40 +0300

    Init cimmit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,28 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDb server.');
+  }
+  console.log('Connected to MongoDb server.');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b7e557073e476d83f6e8037')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos:');
+  //   console.log(JSON.stringify(docs, undefined, 4));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  db.collection('Users').find({
+    name: 'Andrew'
+  }).toArray().then((users) => {
+    console.log(JSON.stringify(users, undefined, 4));
+  }, (err) => {
+    console.log('Unable to fetch todos', err)
+  });
+
+  // db.close();
+});
